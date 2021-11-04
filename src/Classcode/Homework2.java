0a5cb2abf5d2b28b2e9e2b6a3288eef0d85e6e8b
Angelica Vargas <angelicavargas@Angelicas-MBP.fios-router.home>
2021-11-04 19:00:43 -0400

    Homework1

---
+++ src/Classcode/Homework2.java
@@ -0,0 +1,55 @@
+package Classcode;
+
+public class Homework2 {
+
+    public static void main(String[] args) {
+        System.out.println("Homework2");
+
+        double fTemp = 55;
+        double cTemp = (fTemp - 32) * 5/9;
+        System.out.println(fTemp + "°F is equal to " + cTemp + "°C");
+
+        double fT = 55;
+        double kT = (fT + 459.67) * 5/9;
+        System.out.println(fT + "°F is equal to " + kT + "K" );
+
+        double celDegree = 20;
+        double fDegree = (celDegree * 9/5 + 32);
+        System.out.println(celDegree + "°C is equal to " + fDegree + "°F ");
+
+        double cDegree = 20;
+        double kDegree = (cDegree + 273.15);
+        System.out.println(cDegree + "°C is equal to " + kDegree + "K");
+
+        double kTem = 300;
+        double cD = (kTem - 273.15);
+        System.out.println(kTem + "K is equal to " + cD + "°C");
+
+        double kelT = 300;
+        double fahT = (kelT * 9/5 - 459.67);
+        System.out.println(kelT + "K is equal to " + fahT + "°F");
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
---
+++ src/Classcode/PrimitiveDT.java
@@ -0,0 +1,40 @@
+package Classcode;
+
+public class PrimitiveDT {
+
+    public static void main(String[] args) {
+
+        System.out.println("Homework1");
+
+        byte dayHours = 24;
+        System.out.println("\n Total number of hours in a day is " + dayHours);
+
+        short daysAYear = 365;
+        System.out.println("\n Max number of days a year is " + daysAYear) ;
+
+        int numOfEmployees = 1335000;
+        System.out.println("\n Total number of employees in Amazon is " + numOfEmployees);
+
+        long countryPopulation = 333596010L;
+        System.out.println("\n The population number of USA is " + countryPopulation);
+
+        float mortgageRate = 2.4f;
+        System.out.println("\n Mortage Rate = " + mortgageRate);
+
+        double bankAccountBalance = 10.50;
+        System.out.println("\n My Bank Account Balance is = " + bankAccountBalance);
+
+        boolean doesSunRiseWest = false;
+        System.out.println("\n Does the sun rise from the west? = " + false);
+
+        char firstInitial = 'S';
+        char secondInitial = 'G';
+        System.out.println("\n My initials are " + firstInitial + secondInitial);
+
+
+
+
+
+
+    }
+}
